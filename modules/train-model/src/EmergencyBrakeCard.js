088116efff81971bf9dc92fdd24066cd86e32975
brendanjduff <brendanjduff@gmail.com>
2022-03-01 12:18:37 -0500

    Fix: Emergency Brake Card Spacing

--- modules/train-model/src/EmergencyBrakeCard.js
+++ modules/train-model/src/EmergencyBrakeCard.js
@@ -5,9 +5,9 @@ const { ipcRenderer } = window.require('electron')
 export default class EmergencyBrakeCard extends React.Component {
   render () {
     return (
-      <Card className='noVertical'>
+      <Card>
         <Form>
-          <Card.Body>
+          <Card.Body style={{ paddingTop: 13 + 'px', paddingBottom: 13 + 'px'}}>
             <Card.Text>
               <Row className='noVertical'>
                 <Button variant={this.props.ebrake ? 'secondary' : 'danger'} style={{ margin: 0 }} onClick={() => { ipcRenderer.send('setEmergencyBrake', !this.props.ebrake) }}><strong>{this.props.ebrake ? 'Disengage' : 'EMERGENCY BRAKE!'}</strong></Button>
