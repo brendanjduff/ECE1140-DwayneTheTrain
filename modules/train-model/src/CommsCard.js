6d0e17234d3f2dca2eac6556311decc9c5ba926e
brendanjduff <brendanjduff@gmail.com>
2022-04-28 19:31:05 -0400

    Push Final Code

--- modules/train-model/src/CommsCard.js
+++ modules/train-model/src/CommsCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import { msToMph } from './UnitConversion'
 
 export default class CommsCard extends React.Component {
   render () {
@@ -8,9 +9,9 @@ export default class CommsCard extends React.Component {
         <Card.Body>
           <Card.Title>Train Communications</Card.Title>
           <Card.Text>
-            <strong>SpeedCmd: </strong>{this.props.train.thru.speedCmd.toFixed(1)} mph<br />
+            <strong>SpeedCmd: </strong>{msToMph(this.props.train.thru.speedCmd).toFixed(1)} mph<br />
             <strong>AuthorityCmd: </strong>{this.props.train.thru.authorityCmd} blocks<br />
-            <strong>PowerCmd: </strong>{parseFloat(this.props.train.state.powerCmd).toFixed(0)} kJ<br />
+            <strong>PowerCmd: </strong>{parseFloat(this.props.train.state.powerCmd/1000).toFixed(0)} kW<br />
             <strong>Station: </strong>{this.props.train.thru.station ? this.props.train.thru.station : 'None'}<br />
             <strong>Platform: </strong>{(this.props.train.thru.leftPlatform ? 'Left' : '') + ((this.props.train.thru.leftPlatform && this.props.train.thru.rightPlatform) ? '/' : '') + (this.props.train.thru.rightPlatform ? 'Right' : '') + ((!this.props.train.thru.leftPlatform && !this.props.train.thru.rightPlatform) ? 'None' : '')}<br />
             <strong>Underground: </strong>{this.props.train.thru.underground ? 'Yes' : 'No'}<br />
